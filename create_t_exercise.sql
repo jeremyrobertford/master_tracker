cac7b63961934950885385c3a1d9d48303abeec9
jeremyrobertford <45186446+jeremyrobertford@users.noreply.github.com>
2021-03-03 18:31:43 -0500

    Update create_t_exercise.sql
    Converted function to varchar(15) to allow more function values

--- create_t_exercise.sql
+++ create_t_exercise.sql
@@ -2,7 +2,7 @@ CREATE TABLE master_tracker.exercise (
 	ID smallint AUTO_INCREMENT,
 	exercise varchar(100) NOT NULL,
 	difficulty varchar(12) NOT NULL,
-	primary_func char(4) NOT NULL,
+	primary_func varchar(15) NOT NULL,
 	primary_plane varchar(10) NOT NULL,
 	notes varchar(255),
 	CONSTRAINT exercise_pkey PRIMARY KEY (ID)

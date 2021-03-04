a582efdf4fa825c48bfe6511211291628c8c1df1
jeremyrobertford <45186446+jeremyrobertford@users.noreply.github.com>
2021-03-04 12:21:05 -0500

    Update create_t_workout_exercise.sql
    Added ID field as primary key instead of combination of two foreign keys. Changed reps and rest data types to smallint to allow for higher values.

--- create_t_workout_exercise.sql
+++ create_t_workout_exercise.sql
@@ -1,13 +1,14 @@
 CREATE TABLE master_tracker.workout_exercise (
-	workout_id   smallint,
-	exercise_id  smallint,
+	ID           smallint AUTO_INCREMENT,
+	workout_id   smallint NOT NULL,
+	exercise_id  smallint NOT NULL,
 	sets         tinyint NOT NULL,
-	reps         tinyint NOT NULL,
-	rest         tinyint NOT NULL,
+	reps         smallint NOT NULL,
+	rest         smallint NOT NULL,
 	reason       varchar(50) NOT NULL,
 	notes        varchar(255),
 	CONSTRAINT workout_exercise_pkey
-		PRIMARY KEY (workout_id, exercise_id),
+		PRIMARY KEY (ID),
 	CONSTRAINT workout_exercise_workout_id_fkey 
 		FOREIGN KEY (workout_id) REFERENCES master_tracker.workout(ID),
 	CONSTRAINT workout_exercise_exercise_id_fkey 

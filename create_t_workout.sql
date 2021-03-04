568084e4765021e6cb031a3442afa4935be75a71
jeremyrobertford <45186446+jeremyrobertford@users.noreply.github.com>
2021-03-03 19:16:52 -0500

    Update create_t_workout.sql
    Change workout_name to name

--- create_t_workout.sql
+++ create_t_workout.sql
@@ -1,6 +1,6 @@
 CREATE TABLE master_tracker.workout (
 	ID smallint AUTO_INCREMENT,
-	workout_name varchar(100) NOT NULL,
+	name varchar(100) NOT NULL,
 	area_focus char(5) NOT NULL,
 	part_focus char(4) NOT NULL,
 	func_focus varchar(15) NOT NULL,
@@ -9,4 +9,4 @@ CREATE TABLE master_tracker.workout (
 );
 
 CREATE UNIQUE INDEX workout_idx
-ON master_tracker.workout (workout_name);
+ON master_tracker.workout (name);

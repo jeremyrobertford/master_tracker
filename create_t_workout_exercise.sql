2c81e915160346391e11d36842a8ce032462ae97
jeremyrobertford <45186446+jeremyrobertford@users.noreply.github.com>
2021-03-03 22:33:04 -0500

    Update create_t_workout_exercise.sql
    Added reason field and added not null constraints

--- create_t_workout_exercise.sql
+++ create_t_workout_exercise.sql
@@ -1,9 +1,10 @@
 CREATE TABLE master_tracker.workout_exercise (
-	workout_id smallint,
-	exercise_id smallint,
-	sets tinyint,
-	reps tinyint,
-	rest tinyint,
+	workout_id   smallint,
+	exercise_id  smallint,
+	sets         tinyint NOT NULL,
+	reps         tinyint NOT NULL,
+	rest         tinyint NOT NULL,
+	reason       varchar(50) NOT NULL,
 	CONSTRAINT workout_exercise_pkey
 		PRIMARY KEY (workout_id, exercise_id),
 	CONSTRAINT workout_exercise_workout_id_fkey 

f7da037d65241183327a725db009278565c115c0
jeremyrobertford <45186446+jeremyrobertford@users.noreply.github.com>
2021-03-03 21:08:25 -0500

    Update create_t_workout.sql
    Added difficulty to Workout

--- create_t_workout.sql
+++ create_t_workout.sql
@@ -1,6 +1,7 @@
 CREATE TABLE master_tracker.workout (
 	ID smallint AUTO_INCREMENT,
 	name varchar(100) NOT NULL,
+	difficulty varchar(12) NOT NULL,
 	area_focus char(5) NOT NULL,
 	part_focus char(4) NOT NULL,
 	func_focus varchar(15) NOT NULL,

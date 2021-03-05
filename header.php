9ecdb1ce67e2c98cc9cf9d8ce42d90429775e172
jeremyrobertford <45186446+jeremyrobertford@users.noreply.github.com>
2021-03-05 11:17:26 -0500

    Remove title tag in header

--- header.php
+++ header.php
@@ -5,7 +5,6 @@
 
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Choose Option</title>
 
     <link href="resources/css/bootstrap.css" rel="stylesheet" />
     <link href="resources/css/style_template.css" rel="stylesheet" />
